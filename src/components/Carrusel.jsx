2bc52a0dc195211900b1586a8642cdebe1915507
GonzaSotelo <g.sotelo.dab@gmail.com>
2024-09-26 18:05:11 -0300

    imagenes

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -14,21 +14,21 @@ export const Carrusel = () => {
     <>
      <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <img src="img/banner/banner.jpg" className='imagen-carrusel' alt="" />
+        <img src="/img/banner/banner.jpg" className='imagen-carrusel' alt="" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src="img/banner/banner.jpg" className='imagen-carrusel' alt="" />
+      <img src="/img/banner/banner.jpg" className='imagen-carrusel' alt="" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src="img/banner/banner.jpg" className='imagen-carrusel' alt="" />
+      <img src="/img/banner/banner.jpg" className='imagen-carrusel' alt="" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
